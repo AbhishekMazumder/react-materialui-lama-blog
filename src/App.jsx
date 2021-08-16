6c47124d66700c02cab4725985aa4dc729e40da1
abhishek <abhishekmazumder69@gmail.com>
2021-08-16 19:26:38 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import {Button} from '@material-ui/core';
+
+const App = () => {
+  return (
+		<>
+			<h1>Hello world!</h1>
+      <Button color="primary" variant="contained">Primary BTN</Button>
+		</>
+	);
+};
+
+export default App;
